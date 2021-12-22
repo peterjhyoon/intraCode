06eef1d5a394458c6d2f6523cae5ce7192b67578
Peter Yoon <51959999+peterjhyoon@users.noreply.github.com>
2021-12-22 21:21:48 +0900

    Add files via upload

--- intracode.js
+++ intracode.js
@@ -8,22 +8,10 @@ document.addEventListener("keydown", function(event) {
         event.preventDefault();
     }
     if (event.keyCode == 13) {
-        event.preventDefault();
         var newLine = document.createElement("div");
         newLine.className = "code-line";
-        newLine.contentEditable = true;
         lines.after(newLine);
-        // move cursor to next div!!
     }
-    // Up key --> move up a line (div)
-    if (event.keyCode == 38) {
-        console.log();
-    }
-    // Down key
-    if (event.keyCode == 40) {
-        console.log(this);
-    }
-
 });
 
 function getCurrLine(line) {
