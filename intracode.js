1d32268a150d691691c9066762a9f0f7faee84ba
Peter Yoon <51959999+peterjhyoon@users.noreply.github.com>
2021-12-22 13:16:43 +0900

    Add files via upload

--- intracode.js
+++ intracode.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener("keydown", function(event) {
     var lines = document.querySelector(".code-line");
     // Tab for Indent
@@ -13,13 +15,27 @@ document.addEventListener("keydown", function(event) {
         lines.after(newLine);
         // move cursor to next div!!
     }
+    // Up key --> move up a line (div)
+    if (event.keyCode == 38) {
+        console.log();
+    }
+    // Down key
+    if (event.keyCode == 40) {
+        console.log(this);
+    }
+
 });
 
 function getCurrLine(line) {
-    var lines = document.querySelector(".code-line"); 
+    var lines = document.querySelector(".code-line");
+
 }
 
-// Also use for auto-indent
+// For tabbing
 function tabOnCode() {
     document.execCommand('insertHTML', false, '&#009');
+}
+
+function moveCursor() {
+    
 }
