3a01cc0d59f0c2d13c9bfae67429a9f987211312
Peter Yoon <51959999+peterjhyoon@users.noreply.github.com>
2021-12-30 23:43:45 +0900

    Add files via upload

--- intracode.js
+++ intracode.js
@@ -37,34 +37,40 @@ function updateTabs() {
     tabCount++;
     // reference to + button
     var tabBase = document.querySelector(".workspace");
-    var spaceBase = document.querySelector(".code-space");
-    console.log(tabBase);
-
+    var spaceBase = document.querySelector(".supplement-bar");
     // Creating new tab and space
-    //var newSpace = document.createElement("div");
+    var newSpace = document.createElement("div");
     var newTab = document.createElement("div");
-    //newSpace.className = "code-space";
+    newSpace.className = "code-space";
     newTab.className = "workspace-tab";
-    //newSpace.setAttribute('contenteditable',true);
-    //newSpace.setAttribute('tab', tabCount);
+    newSpace.setAttribute('contenteditable',true);
+    newSpace.setAttribute('tab', tabCount);
 
     // change name context (start with no num, then with num)
-    newTab.textContent = "Untitled-" + tabCount;
-    newTab.setAttribute("tab-num",tabCount+1);
+    if (tabCount == 1) {
+        newTab.textContent = "Untitled";
+    }
+    else {
+        newTab.textContent = "Untitled-" + tabCount;
+    }
+    //newTab.textContent = "Untitled-" + tabCount;
+    newTab.setAttribute("tab-num",tabCount);
     var closeButton = document.createElement("button");
     closeButton.className = "close-tab";
     closeButton.textContent = "x";
-    closeButton.setAttribute("button-num",tabCount+1);
-    // Deleting Tab
+    closeButton.setAttribute("button-num",tabCount);
+    // Deleting Tab and Space
     closeButton.onclick = function() {
         // make sure to remove corresponding codespace
         var temp = tabBase.firstElementChild;
         let i = 0
         while (i < closeButton.getAttribute('button-num')) {
             temp = temp.nextElementSibling;
-            console.log(temp);
+            //console.log(temp);
             if (temp.getAttribute('tab-num') == closeButton.getAttribute('button-num')) {
                 tabBase.removeChild(temp);
+                document.body.removeChild(newSpace);
+                tabCount--;
                 // codespace object.style.display = "none"
                 break;
             }
@@ -72,9 +78,45 @@ function updateTabs() {
         }
     }
     newTab.appendChild(closeButton);
+    if (tabCount >= 2) {
+        newTab.onclick = displayCodeSpace(newTab.getAttribute('tab-num'));
+    }
+    
+    //displayCodeSpace(newTab.getAttribute('tab-num'));
     tabBase.append(newTab);
+    // Adding code lines to new space
+    line = document.createElement("div");
+    line.className = "code-line";
+    newSpace.append(line);
 
+    spaceBase.insertAdjacentElement('beforebegin', newSpace);
     /*
         Make sure that Code Space appends appropriately!!!
     */
 }
+
+// For First Tab (Edge Case)
+if (tabCount > 1) {
+    var firstTab = document.querySelector(".code-space");
+    firstTab.onclick = displayCodeSpace(firstTab.getAttribute('tab-num'));
+}
+
+function displayCodeSpace(tabNum) {
+    var spaces = document.querySelector(".code-space");
+    let counter = 1;
+    while (counter <= tabCount) {
+        if (counter == tabNum) {
+            spaces.style.display = 'block';
+            //console.log("this is the tab to show");
+        }
+        else {
+            spaces.style.display = 'none';
+            //console.log("this is the tab to hide");
+        }
+        spaces = spaces.nextElementSibling;
+        counter++;
+    }
+}
+
+// make sure that the style thing applies *even after last tab closes
+// ie) switch to next open tab (edge case)
